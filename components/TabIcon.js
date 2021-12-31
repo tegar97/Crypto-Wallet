13e9b357d58edae401b5e4e3e9dd18a2ebca25b5
Muhammad Tegar Akmal <eategar@gmail.com>
2021-12-31 19:18:31 +0700

    start project

---
+++ components/TabIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+const TabIcon = () => {
+  return (
+    <View>
+      <Text>Tab</Text>
+    </View>
+  );
+};
+
+export default TabIcon;
